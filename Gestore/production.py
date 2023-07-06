463eff4968a4f91dad9891adf579652597089b25
s13g <ayflix0@gmail.com>
2023-07-06 22:36:40 +0100

    setting up

---
+++ Gestore/production.py
@@ -0,0 +1,36 @@
+import dj_database_url
+from .settings import *
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": config("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": config("CLOUDINARY_API_KEY"),
+    "API_SECRET": config("CLOUDINARY_API_SECRET"),
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+            default=config("DATABASE_URL"),
+            conn_max_age=600,
+            conn_health_checks=True,
+    )
+}
+
+INSTALLED_APPS.remove("debug_toolbar")
+
+EMAIL_USE_TLS = True
+
+EMAIL_USE_SSL = False
+
+EMAIL_HOST = "smtp.gmail.com"
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+EMAIL_PORT = 587
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+MIDDLEWARE.remove("debug_toolbar.middleware.DebugToolbarMiddleware")
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    username = None
+    email_address = models.EmailField(unique=True)
+    
+
